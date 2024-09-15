882e8b1b1b8feff8ffb0f3ee373a194469db37d5
amitm144 <87026850+amitm144@users.noreply.github.com>
2024-09-15 16:45:39 +0300

    add env

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = process.env.API_URL;
 
 async function apiCall(endpoint, method = "GET", data = null, signal = null) {
   const url = `${BASE_URL}${endpoint}`;
